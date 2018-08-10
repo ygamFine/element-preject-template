93c64af27480fe6f25fe86e56cfd3ed318f9ff4f
zhangyun <ygamfine@126.com>
2018-08-10 16:15:57 +0800

    创建项目模板

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Hello World
+# 前端项目模板
+
+> Vue.js + ElementUI + Webpack
+
+## 项目运行
+
+``` bash
+# 安装依赖
+npm install
+
+# 本地运行项目
+gulp
+
+# 需要与后台对接（服务器部署）
+gulp build
+
+build 结束后 生成 'static' 文件夹
+
+```
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+// 运行项目
+gulp.task('default', shell.task([
+  'npm start'
+]));
+
+gulp.task('dev', shell.task([
+  'npm run dev'
+]));
+
+// 项目编译
+gulp.task('build', shell.task([
+   'npm run build'
+]));
+// 编译后提交
+gulp.task('gls', shell.task([
+   'git add -A && git commit -m"编译文件" && git pull && git push'
+]));
